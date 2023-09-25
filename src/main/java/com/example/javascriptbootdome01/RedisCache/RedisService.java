28974e28f89a08849418d8d4ed921191b7c47fec
AAlxp <1452948876@qq.com>
2023-09-25 22:52:41 +0800

    第六章  Spring Boot 默认缓存体验

--- src/main/java/com/example/javascriptbootdome01/RedisCache/RedisService.java
+++ src/main/java/com/example/javascriptbootdome01/RedisCache/RedisService.java
@@ -1,40 +1,40 @@
-package com.example.javascriptbootdome01.RedisCache;
-
-import com.example.javascriptbootdome01.SQL.jpa.Discuss;
-import com.example.javascriptbootdome01.SQL.jpa.DiscussRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-/**
- * @author 李晓鹏
- * @date 2023/9/23 12:08
- * @description:
- */
-@Service
-public class RedisService {
-    @Autowired
-    private DiscussRepository mDiscussRepository;
-
-    @Cacheable(cacheNames = "comment", unless = "#result==null")
-    public Discuss findById(int comment_id) {
-        Optional<Discuss> optional = mDiscussRepository.findById(comment_id);
-        if (optional.isPresent()) {
-            return optional.get();
-        }
-        return null;
-    }
-    @CachePut(cacheNames = "comment",key = "#result.id")
-    public Discuss updateComment(Discuss comment){
-        mDiscussRepository.updateDiscuss(comment.getAuthor(),comment.getAid());
-        return comment;
-    }
-    @CacheEvict(cacheNames = "comment")
-    public void deleteComment(int comment_id) {
-        mDiscussRepository.deleteById(comment_id);
-    }
-}
+//package com.example.javascriptbootdome01.RedisCache;
+//
+//import com.example.javascriptbootdome01.SQL.jpa.Discuss;
+//import com.example.javascriptbootdome01.SQL.jpa.DiscussRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cache.annotation.CacheEvict;
+//import org.springframework.cache.annotation.CachePut;
+//import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+///**
+// * @author 李晓鹏
+// * @date 2023/9/23 12:08
+// * @description:
+// */
+//@Service
+//public class RedisService {
+//    @Autowired
+//    private DiscussRepository mDiscussRepository;
+//
+//    @Cacheable(cacheNames = "comment", unless = "#result==null")
+//    public Discuss findById(int comment_id) {
+//        Optional<Discuss> optional = mDiscussRepository.findById(comment_id);
+//        if (optional.isPresent()) {
+//            return optional.get();
+//        }
+//        return null;
+//    }
+//    @CachePut(cacheNames = "comment",key = "#result.id")
+//    public Discuss updateComment(Discuss comment){
+//        mDiscussRepository.updateDiscuss(comment.getAuthor(),comment.getAid());
+//        return comment;
+//    }
+//    @CacheEvict(cacheNames = "comment")
+//    public void deleteComment(int comment_id) {
+//        mDiscussRepository.deleteById(comment_id);
+//    }
+//}
